beec52be14c47b0495d819b54b80715615d8e67d
Ivano <Ivano@Ivano-PC>
2020-04-07 23:29:00 -0300

    Atualização do Exercicio

--- src/main/java/view/exercicio1/TelaExclusaoEndereco.java
+++ src/main/java/view/exercicio1/TelaExclusaoEndereco.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class TelaExclusaoEndereco extends JFrame {
 
@@ -34,11 +39,33 @@ public class TelaExclusaoEndereco extends JFrame {
 	public TelaExclusaoEndereco() {
 		setTitle("Exclus\u00E3o de Endere\u00E7o");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 366, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(null);
+		
+		
+		
+		ArrayList<String> Clientes = ConsultarClientes();
+		
+		JComboBox cbClientes = new JComboBox(Clientes.toArray());
+		cbClientes.setBounds(10, 38, 139, 24);
+		contentPane.add(cbClientes);
+		
+		JLabel lblSelecioneOCliente = new JLabel("Selecione o Cliente :");
+		lblSelecioneOCliente.setBounds(21, 11, 128, 14);
+		contentPane.add(lblSelecioneOCliente);
 	}
 
+	private ArrayList<String> ConsultarClientes() {
+		
+		ArrayList<String> Clientes = new ArrayList<String>();
+		Clientes.add("Ivano");
+		Clientes.add("Vilmar");
+		Clientes.add("João");
+		Clientes.add("Pedro");
+		
+				return Clientes;
+	}
 }
